5ca78cc8910ad40d0249099afb46034adc897511
isaacmusa <zicthngz@gmail.com>
2023-04-18 09:40:43 +0100

    34343

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
+"""
+Lists all states with a name starting with N (upper N)
+It takes 3 arguments: mysql username, mysql password and database name
+"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    query = "SELECT * FROM states\
+             WHERE states.name LIKE BINARY 'N%'\
+             ORDER BY states.id ASC"
+    cursor = db.cursor()
+    cursor.execute(query)
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
-import sys
+"""
+Filter cities by state input safe from MySQL injections!
+takes in the name of a state as an argument and lists all cities of that state
+"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    query = "SELECT cities.name\
+             FROM cities JOIN states\
+             ON cities.state_id = states.id\
+             WHERE states.name = %s"
+    cursor = db.cursor()
+    cursor.execute(query, (argv[4], ))
+    cities = []
+    for city in cursor.fetchall():
+        cities.append(city[0])
+    print(", ".join(cities))
+    cursor.close()
+    db.close()
